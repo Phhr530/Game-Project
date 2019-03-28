c6a986017942af5b2af5b78b50a19d932c3630de
Parker robinson <phhr530@outlook.com>
2019-03-28 15:05:10 -0700

    i made new colors and new diameter

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -4,8 +4,8 @@ let anotherBall;
 
 function setup() {
   createCanvas(800, 400);
-  b = new Ball(0, 100,"red"); //make a new ball from the Ball class and call it b.
-  anotherBall = new Ball(200,20,"green");
+  b = new Ball(0, 100,"red",10); //make a new ball from the Ball class and call it b.
+  anotherBall = new Ball(200,20,"green",25);
 }
 
 
@@ -22,15 +22,17 @@ function draw(){
 //ball class from which to create new balls with similar properties.
 class Ball {
 
-	constructor(x,y,color){ //every ball needs an x value and a y value
+	constructor(x,y,color,diameter){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
         this.color= color;
+        this.diameter= diameter;
 	}
 	drawBall(){  // draw a ball on the screen at x,y
     		stroke(0);
     		fill(this.color);
-		    ellipse(this.x,this.y,10,10);
+		    ellipse(this.x,this.y,this.diameter,this.diameter);
+
 	}
 	moveBall(){ //update the location of the ball, so it moves across the screen
 		this.x = this.x+2;
